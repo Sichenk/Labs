affe6d81e37680072ef8a91fb4d3b7cbe09d60be
Sichenk <140361877+Sichenk@users.noreply.github.com>
2023-11-15 18:27:58 +0700

    Update Prakt_2.c

--- Prakt_2.c
+++ Prakt_2.c
@@ -55,20 +55,20 @@ int secondHash(char* str) {
 }
 
 // Множество
-void SADD(Set* set, char* value);
-void SREM(Set* set, char* value);
+void SADD(Set* set, char* value, SOCKET* clientSocket);
+void SREM(Set* set, char* value, SOCKET* clientSocket);
 int SISMEMBER(Set* set, char* value);
 
 // Стек
-void SPUSH(Stack* stack, char* value);
+void SPUSH(Stack* stack, char* value, SOCKET* clientSocket);
 char* SPOP(Stack* stack);
 
 // Очередь
-void QPUSH(Queue* queue, char* value);
+void QPUSH(Queue* queue, char* value, SOCKET* clientSocket);
 char* QPOP(Queue* queue);
 
 // Хэш-таблица
-void HSET(HashTable* hashtable, char* key, char* value);
+void HSET(HashTable* hashtable, char* key, char* value, SOCKET* clientSocket);
 char* HDEL(HashTable* hashtable, char* key);
 char* HGET(HashTable* hashtable, char* key);
 
@@ -114,7 +114,7 @@ void removeNewline(char* str) {
     }
 }
 
-void copyingValuesFromFile(FILE* file, Set* set, Stack* stack, Queue* queue, HashTable* hashTable) {
+void copyingValuesFromFile(FILE* file, Set* set, Stack* stack, Queue* queue, HashTable* hashTable, SOCKET* client_socket) {
     char line[256];
     long set_current_pos = -1, stack_current_pos = -1, queue_current_pos = -1, hashtable_current_pos = -1;
     while (fgets(line, sizeof(line), file) != NULL) {
@@ -135,7 +135,7 @@ void copyingValuesFromFile(FILE* file, Set* set, Stack* stack, Queue* queue, Has
     fseek(file, set_current_pos, SEEK_SET);
     while (fgets(line, sizeof(line), file) != NULL && ftell(file) < stack_current_pos) {
         removeNewline(line);
-        SADD(set, line);
+        SADD(set, line, client_socket);
     }
     fseek(file, stack_current_pos, SEEK_SET);
     char stackData[SIZE][256];
@@ -146,12 +146,12 @@ void copyingValuesFromFile(FILE* file, Set* set, Stack* stack, Queue* queue, Has
         stackDataIndex++;
     }
     for (int i = stackDataIndex - 1; i >= 0; i--) {
-        SPUSH(stack, stackData[i]);
+        SPUSH(stack, stackData[i], client_socket);
     }
     fseek(file, queue_current_pos, SEEK_SET);
     while (fgets(line, sizeof(line), file) != NULL && ftell(file) < hashtable_current_pos) {
         removeNewline(line);
-        QPUSH(queue, line);
+        QPUSH(queue, line, client_socket);
     }
     fseek(file, hashtable_current_pos, SEEK_SET);
     char* key = NULL;
@@ -163,7 +163,7 @@ void copyingValuesFromFile(FILE* file, Set* set, Stack* stack, Queue* queue, Has
         }
         else {
             char* value = _strdup(line);
-            HSET(hashTable, key, value);
+            HSET(hashTable, key, value, client_socket);
             free(key);
             key = NULL;
             free(value);
@@ -188,13 +188,16 @@ void copyingValuesFromFile(FILE* file, Set* set, Stack* stack, Queue* queue, Has
             // Prakt_2 --file C:\data.txt --query HDEL key1
             // Prakt_2 --file C:\data.txt --query HGET key1
 
-void queryProcessing(int argc, char* argv[]) {
+void queryProcessing(int argc, char* argv[], SOCKET* client_socket) {
+    // Создание мьютекса для безопасного доступа к базе данных
     dbMutex = CreateMutex(NULL, FALSE, NULL);
     if (dbMutex == NULL) {
         printf("Mutex creation error.\n");
         return -1;
     }
+    // Ожидание блокировки мьютекса
     WaitForSingleObject(dbMutex, INFINITE);
+    // Проверка корректности аргументов командной строки
     if (argc < 4 || strcmp(argv[1], "--file") != 0 || strcmp(argv[3], "--query") != 0) {
         printf("Usage: %s --file <path_to_file> --query <query>\n", argv[0]);
         return -1;
@@ -221,7 +224,7 @@ void queryProcessing(int argc, char* argv[]) {
         printf("File opening error.\n");
         return -1;
     }
-
+    // Инициализация структур данных
     Set set;
     set.count = 0;
     memset(set.elements, 0, sizeof(set.elements));
@@ -236,100 +239,136 @@ void queryProcessing(int argc, char* argv[]) {
     hashTable.count = 0;
     memset(hashTable.keys, 0, sizeof(hashTable.keys));
     memset(hashTable.values, 0, sizeof(hashTable.values));
+    // Обработка запроса
     if (strcmp(query, "SADD") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
-        SADD(&set, argv[5]);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
+        SADD(&set, argv[5], client_socket);
+        send(client_socket, "Done!\n", sizeof("Done!\n"), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else if (strcmp(query, "SREM") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
-        SREM(&set, argv[5]);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
+        SREM(&set, argv[5], client_socket);
+        send(client_socket, "Done!\n", sizeof("Done!\n"), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else if (strcmp(query, "SISMEMBER") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
         if (SISMEMBER(&set, argv[5]) == 1) {
             printf("TRUE\n");
+            send(client_socket, "TRUE\n", sizeof("TRUE\n"), 0);
         }
         else {
             printf("FALSE\n");
+            send(client_socket, "FALSE\n", sizeof("FALSE\n"), 0);
         }
     }
     else if (strcmp(query, "SPUSH") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
-        SPUSH(&stack, argv[5]);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
+        SPUSH(&stack, argv[5], client_socket);
+        send(client_socket, "Done!\n", sizeof("Done!\n"), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else if (strcmp(query, "SPOP") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
         char* popped_item = SPOP(&stack);
         printf("Popped item: %s\n", popped_item);
+        char message[32];
+        sprintf(message, "Popped item: %s\n", popped_item);
+        send(client_socket, message, strlen(message), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else if (strcmp(query, "QPUSH") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
-        QPUSH(&queue, argv[5]);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
+        QPUSH(&queue, argv[5], client_socket);
+        send(client_socket, "Done!\n", sizeof("Done!\n"), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else if (strcmp(query, "QPOP") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
         char* popped_item = QPOP(&queue);
         printf("Popped item: %s\n", popped_item);
+        char message[32];
+        sprintf(message, "Popped item: %s\n", popped_item);
+        send(client_socket, message, strlen(message), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else if (strcmp(query, "HSET") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
-        HSET(&hashTable, argv[5], argv[6]);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
+        HSET(&hashTable, argv[5], argv[6], client_socket);
+        send(client_socket, "Done!\n", sizeof("Done!\n"), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else  if (strcmp(query, "HDEL") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
         char* deleted_item = HDEL(&hashTable, argv[5]);
         printf("Deleted item: %s\n", deleted_item);
+        char message[32];
+        sprintf(message, "Deleted item: %s\n", deleted_item);
+        send(client_socket, message, strlen(message), 0);
         fileUpdate(file, filename, &set, &stack, &queue, &hashTable);
     }
     else  if (strcmp(query, "HGET") == 0) {
-        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable);
+        copyingValuesFromFile(file, &set, &stack, &queue, &hashTable, client_socket);
         char* value = HGET(&hashTable, argv[5]);
         printf("Value: %s\n", value);
+        char message[32];
+        sprintf(message, "Value: %s\n", value);
+        send(client_socket, message, strlen(message), 0);
     }
+    // Закрытие файла и освобождение мьютекса
     fclose(file);
     ReleaseMutex(dbMutex);
     CloseHandle(dbMutex);
+    // Освобождение памяти от аргументов командной строки
     for (int i = 0; i < 7; i++) {
         free(argv[i]);
     }
 }
 
 DWORD WINAPI threadFunction(LPVOID lpParam) {
+    // Передача аргументов потока
     ThreadArgs* args = (ThreadArgs*)lpParam;
+
+    // Получение сокетов
     SOCKET* s = args->serverSocket;
     SOCKET* client_socket = args->clientSocket;
+
+    // Буфер для приема данных от клиента
     char buffer[20];
     char* argv[7];
     int argc = 7;
     memset(argv, 0, sizeof(argv));
 
+
+    // Основной цикл обработки запросов от клиента
     while (1) {
+        // Прием сообщения от клиента
         char working_buffer[40];
         memset(working_buffer, 0, sizeof(working_buffer));
         recv(client_socket, working_buffer, sizeof(working_buffer), 0);
+
+        // Обработка сообщения от клиента
         if (strcmp(working_buffer, "Client disconnected!\n") == 0) {
             printf("%s", working_buffer);
         }
         else if (strcmp(working_buffer, "Client keep working!\n") == 0) {
+            // Прием аргументов запроса от клиента
             for (int i = 0; i < 7; i++) {
                 recv(client_socket, buffer, sizeof(buffer), 0);
                 argv[i] = _strdup(buffer);
             }
+            // Отправка подтверждения клиенту
             char ready_buffer[10] = "Done!\n";
             send(client_socket, ready_buffer, sizeof(ready_buffer), 0);
+            // Вывод аргументов запроса на экран
             printf("Client answer: ");
             for (int i = 0; i < 7; i++) {
                 printf("%s ", argv[i]);
             }
             printf("\n");
-            queryProcessing(argc, argv);
+            // Вызов функции обработки запроса
+            queryProcessing(argc, argv, client_socket);
         }
     }
     return 0;
@@ -337,12 +376,15 @@ DWORD WINAPI threadFunction(LPVOID lpParam) {
 
 int main() {
     setlocale(LC_ALL, "Rus");
+    // Инициализация библиотеки Winsock
     WSADATA ws;
     WSAStartup(MAKEWORD(2, 2), &ws);
 
+    // Создание серверного сокета
     SOCKET s;
     s = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Инициализация структуры сервера
     SOCKADDR_IN server;
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
@@ -354,7 +396,7 @@ int main() {
     printf("-----------------------\n");
     scanf_s("%c", &c);
 
-    // Prakt_2 --file C:\data.txt --query SADD item
+    // Клиентский режим
     if (c == 'k') {
         server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
         if (connect(s, &server, sizeof(server)) == -1) {
@@ -388,11 +430,24 @@ int main() {
                 recv(s, ready_buffer, sizeof(ready_buffer), 0);
                 printf(ready_buffer);
 
+                Sleep(1000);
+                char answer_buffer[32];
+                memset(answer_buffer, 0, sizeof(answer_buffer));
+
+                // Принимаем данные от сервера
+                int bytesReceived = recv(s, answer_buffer, sizeof(answer_buffer), 0);
+                if (bytesReceived > 0) {
+                    answer_buffer[bytesReceived] = '\n';
+                    printf("Server answer: %s\n", answer_buffer);
+
+                }
+
                 Sleep(3000);
             }
         }
 
     }
+    // Серверный режим
     if (c == 's') {
         bind(s, &server, sizeof(server));
         listen(s, 100);
@@ -419,13 +474,13 @@ int main() {
             }
         }
     }
-
+    // Закрытие сокета и освобождение ресурсов Winsock
     closesocket(s);
     WSACleanup();
 }
 
 // Множество
-void SADD(Set* set, char* value) {
+void SADD(Set* set, char* value, SOCKET* client_socket) {
     if (set->count < SIZE) {
         int index = hash(value);
         if (set->elements[index] == NULL) {
@@ -434,14 +489,16 @@ void SADD(Set* set, char* value) {
         }
         else {
             printf("This cell is already employed.\n");
+            send(client_socket, "This cell is already employed.\n", sizeof("This cell is already employed.\n"), 0);
         }
     }
     else {
         printf("Set is full.\n");
+        send(client_socket, "Set is full.\n", sizeof("Set is full.\n"), 0);
     }
 }
 
-void SREM(Set* set, char* value) {
+void SREM(Set* set, char* value, SOCKET* client_socket) {
     int index = hash(value);
     if (set->elements[index] != NULL) {
         free(set->elements[index]);
@@ -450,6 +507,7 @@ void SREM(Set* set, char* value) {
     }
     else {
         printf("There is no such element.\n");
+        send(client_socket, "There is no such element.\n", sizeof("There is no such element.\n"), 0);
     }
 }
 
@@ -464,13 +522,14 @@ int SISMEMBER(Set* set, char* value) {
 }
 
 // Стек
-void SPUSH(Stack* stack, char* value) {
+void SPUSH(Stack* stack, char* value, SOCKET* client_socket) {
     if (stack->top < SIZE - 1) {
         stack->top++;
         stack->elements[stack->top] = _strdup(value);
     }
     else {
         printf("Stack is full.\n");
+        send(client_socket, "Stack is full.\n", sizeof("Stack is full.\n"), 0);
     }
 }
 
@@ -486,13 +545,14 @@ char* SPOP(Stack* stack) {
 }
 
 // Очередь
-void QPUSH(Queue* queue, char* value) {
+void QPUSH(Queue* queue, char* value, SOCKET* client_socket) {
     if (queue->back < SIZE - 1) {
         queue->elements[queue->back] = _strdup(value);
         queue->back++;
     }
     else {
         printf("Queue is full.\n");
+        send(client_socket, "Queue is full.\n", sizeof("Queue is full.\n"), 0);
     }
 }
 
@@ -508,7 +568,7 @@ char* QPOP(Queue* queue) {
 }
 
 // Хэш-таблица (теперь есть обработка коллизий через двойной хэш)
-void HSET(HashTable* hashtable, char* key, char* value) {
+void HSET(HashTable* hashtable, char* key, char* value, SOCKET* client_socket) {
     if (hashtable->count < SIZE) {
         int index = hash(key);
         int step = secondHash(key);
@@ -525,10 +585,13 @@ void HSET(HashTable* hashtable, char* key, char* value) {
         }
         else {
             printf("Key already exists.\n");
+            send(client_socket, "Key already exists.\n", sizeof("Key already exists.\n"), 0);
+
         }
     }
     else {
         printf("HashTable is full.\n");
+        send(client_socket, "HashTable is full.\n", sizeof("HashTable is full.\n"), 0);
     }
 }
 
