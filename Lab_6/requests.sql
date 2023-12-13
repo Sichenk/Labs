7f00d604e6666d00c2dc5960a4831cc34eaee89c
Sichenk <140361877+Sichenk@users.noreply.github.com>
2023-12-13 15:58:15 +0700

    Update requests.sql

--- Lab_6/requests.sql
+++ Lab_6/requests.sql
@@ -1,4 +1,5 @@
-SELECT * FROM developers
+-- Active: 1702374532296@@127.0.0.1@3306@games
+SELECT * FROM games
 
 SELECT 
 `GameID` AS `id`,
@@ -43,7 +44,7 @@ WHERE `Title` LIKE 'the%' OR `ReleaseYear` > 2019
 
 SELECT *
 FROM games
-ORDER BY ``ReleaseYear`` ASC
+ORDER BY `ReleaseYear`
 
 SELECT *
 FROM games
@@ -73,3 +74,30 @@ JOIN
     gamedevelopers ON gamedevelopers.GameID = games.GameID
 JOIN 
     developers ON developers.DeveloperID = gamedevelopers.DeveloperID
+
+UPDATE `storage`
+SET `DiskSpaceGB` = 5
+WHERE `GameID` = 5
+
+SELECT 
+    games.Title, 
+    `storage`.DiskSpaceGB,
+    userreviews.`Rating`,
+    graphics.`GraphicsQuality`
+FROM 
+    games
+JOIN
+    `storage` ON `storage`.`GameID`= games.GameID
+JOIN
+    userreviews ON userreviews.`GameID`= games.GameID
+JOIN
+    graphics ON graphics.`GameID`= games.GameID
+WHERE userreviews.`Rating` >= 4
+ORDER BY userreviews.`Rating` DESC
+LIMIT 3
+
+SELECT Genre, COUNT(*) `counter`
+FROM Games
+GROUP BY Genre
+ORDER BY `counter` DESC
+
